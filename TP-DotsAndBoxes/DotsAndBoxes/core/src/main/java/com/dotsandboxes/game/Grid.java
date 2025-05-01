46bf3b094dc6ca169aeea763d3d7ca54c4165e29
Alessandro Kennedy <136772341+AlessandroKSR@users.noreply.github.com>
2025-05-01 19:44:19 -0300

    Add files via upload

---
+++ TP-DotsAndBoxes/DotsAndBoxes/core/src/main/java/com/dotsandboxes/game/Grid.java
@@ -0,0 +1,11 @@
+package com.dotsandboxes.game;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Grid {
+    public ShapeRenderer quadrado;
+
+    public void create() {
+        quadrado = new ShapeRenderer();
+    }
+}
