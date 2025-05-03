e2ead8ed6d294865e7bdbbffee4f004f997f06f1
Alessandro Kennedy <136772341+AlessandroKSR@users.noreply.github.com>
2025-05-03 18:12:25 -0300

    Visuais da grade quase completos
    Falta a mecânica de preencher as celulas da grade de acordo com quem fechou o quadrado da grade

--- DotsAndBoxes/core/src/main/java/com/dotsandboxes/game/Main.java
+++ DotsAndBoxes/core/src/main/java/com/dotsandboxes/game/Main.java
@@ -1,28 +1,22 @@
 package com.dotsandboxes.game;
 
-import java.util.Random;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/**
- * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
- * platforms.
- */
 public class Main implements ApplicationListener {
     Grade[] celulas;
-    Bola bola;
+    Bola bolas;
     FitViewport viewport;
-    Random random = new Random();
+    Mouse myMouse;
 
     @Override
     public void create() {
         celulas = new Grade[25];
-        bola = new Bola();
-        bola.create();
-
+        myMouse = new Mouse();
+        bolas = new Bola();
+        bolas.create();
         for(int i = 0; i < 25; i++)
         {
             celulas[i] = new Grade();
@@ -35,53 +29,50 @@ public class Main implements ApplicationListener {
 
     @Override
     public void resize(int width, int height) {
-        // Resize your application here. The parameters represent the new window size.
         viewport.update(width, height);
     }
 
     @Override
     public void render() {
         draw();
-    }
-
-    private void input() {
-
+        logic();
+        bolas.criarGrade();
     }
 
     private void logic() {
-
+        myMouse.setMousePosition(viewport);
+        for(int i = 0; i < 25; i++) {
+            celulas[i].mouseHover(myMouse.getMousePosicao());
+        }
+        
     }
 
+    
     private void draw() {
         ScreenUtils.clear(Color.BLACK);
         viewport.apply();
-        
+
+        celulas[0].setContadorGrade(0);
         for(int i = 0; i < 5; i++)
         {
-            for(int j = 0; j < 5;j++)
+            for(int j = 0; j < 5; j++)
             {
-                celulas[j].drawQuadrado(viewport, 1.5F + j, 1.5F + i);;
+                celulas[celulas[0].getContadorGrade()].drawQuadrado(viewport, j + 1.5f, i + 1.5f);
+                celulas[0].updateContadorGrade();
             }
-
-            
         }
-       
-        bola.criarGrade(random);
         
     }
 
     @Override
     public void pause() {
-        // Invoked when your application is paused.
     }
 
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
     }
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
     }
 }
